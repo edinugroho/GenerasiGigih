b7fa39693ac1ea5e312f14c9c5162f35f390f6b0
edinugroho <edidwinugroho80@gmail.com>
2021-07-18 19:50:29 +0700

    add query exercise

--- Module 2/Session 2/food_oms_db.sql
+++ Module 2/Session 2/food_oms_db.sql
@@ -22,3 +22,34 @@ INSERT INTO items(id, name, price) VALUES (4, 'Green Tea Latte', 18000);
 ALTER TABLE items MODIFY id INT NOT NULL auto_increment;
 
 INSERT INTO items(name, price) VALUES ('Green Tea Latte', 18000);
+
+-- 1
+select items.id, items.name, items.price, categories.name from item_categories 
+right join items  
+on items.id = item_categories.item_id  
+left join categories  
+on categories.id = item_categories.category_id;
+
+-- 2
+select items.id, items.name, items.price, categories.name from item_categories 
+right join items 
+on items.id = item_categories.item_id 
+left join categories 
+on categories.id = item_categories.category_id 
+where categories.name = 'main dish';
+
+-- 3
+select items.id, items.name, items.price from item_categories 
+right join items 
+on items.id = item_categories.item_id 
+left join categories 
+on categories.id = item_categories.category_id 
+where categories.name = 'main dish' and items.price > 30000;
+
+-- 4
+select items.id, items.name, items.price from item_categories  
+right join items  
+on items.id = item_categories.item_id  
+left join categories  
+on categories.id = item_categories.category_id  
+where item_categories.category_id is null;
