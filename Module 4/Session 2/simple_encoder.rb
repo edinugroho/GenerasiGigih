f675db9b934270aa3b57d8f0fb83d14d0a7d0ba5
edinugroho <edidwinugroho80@gmail.com>
2021-07-28 18:09:32 +0700

    encode b to k

--- Module 4/Session 2/simple_encoder.rb
+++ Module 4/Session 2/simple_encoder.rb
@@ -1,5 +1,11 @@
 class SimpleEncoder
-    def encode
-        ''
+    def encode(input)
+        if input != ''
+            ord = input.ord
+            new_ord = ord + 9
+            new_ord.chr
+        else
+            ''
+        end
     end
 end
--- Module 4/Session 2/simple_encoder_spec.rb
+++ Module 4/Session 2/simple_encoder_spec.rb
@@ -3,7 +3,14 @@ require_relative './simple_encoder.rb'
 describe SimpleEncoder do
     it 'should return empty string' do
         simple_encoder = SimpleEncoder.new
-        encode = simple_encoder.encode
+        encode = simple_encoder.encode('')
         expect(encode).to eq('')
     end
+
+    it 'should encode b to k' do
+        simple_encoder = SimpleEncoder.new
+        encode = simple_encoder.encode('b')
+        expect(encode).to eq('k')
+    end
+
 end
