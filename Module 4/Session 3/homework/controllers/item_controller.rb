6af0588c1423fa167f83eb04eafc4a248bdf189b
edinugroho <edidwinugroho80@gmail.com>
2021-08-05 01:00:32 +0700

    complete item controllers

--- Module 4/Session 3/homework/controllers/item_controller.rb
+++ Module 4/Session 3/homework/controllers/item_controller.rb
@@ -13,4 +13,31 @@ class ItemController
         render = ERB.new(File.read("./views/create_item.erb"))
         render.result(binding)
     end
+
+    def self.show(id)
+        item = Item.find(id)[0]
+        render = ERB.new(File.read("./views/item_details.erb"))
+        render.result(binding)
+    end
+
+    def self.edit(id)
+        item = Item.find(id)[0]
+        categories = Category.all
+        render = ERB.new(File.read("./views/item_update.erb"))
+        render.result(binding)
+    end
+
+    def self.destroy(id)
+        item = Item.find(id)[0]
+        item.delete
+    end
+
+    def self.store(params)
+        item = Item.new({
+            'name' => params['name'],
+            'price' => params['price'],
+            'categories' => params['categories']
+        })
+        item.save
+    end
 end
